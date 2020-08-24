904c398e0ed6e92f9182c98e06bcd2ac823e57b7
BrandonStalnaker <bstalnaker@mparticle.com>
2020-08-24 15:33:54 -0400

    Update for iOS 14

---
+++ mParticle-Apptimize/mParticle_Apptimize.h
@@ -0,0 +1,18 @@
+//
+//  mParticle_Apptimize.h
+//  mParticle-Apptimize
+//
+//  Created by Brandon Stalnaker on 8/24/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mParticle_Apptimize.
+FOUNDATION_EXPORT double mParticle_ApptimizeVersionNumber;
+
+//! Project version string for mParticle_Apptimize.
+FOUNDATION_EXPORT const unsigned char mParticle_ApptimizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mParticle_Apptimize/PublicHeader.h>
+
+
