787c99c1b583022addec9cb3699f79928ef64f32
BrandonStalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2020-09-14 09:54:39 -0400

    Update mParticle-Apptimize/mParticle_Apptimize.h
    Co-authored-by: Peter Jenkins <pjenkins@mparticle.com>

--- mParticle-Apptimize/mParticle_Apptimize.h
+++ mParticle-Apptimize/mParticle_Apptimize.h
@@ -14,5 +14,4 @@ FOUNDATION_EXPORT double mParticle_ApptimizeVersionNumber;
 FOUNDATION_EXPORT const unsigned char mParticle_ApptimizeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <mParticle_Apptimize/PublicHeader.h>
-
-
+#import <mParticle_Apptimize/MPKitApptimize.h>
